5236e1f60daa4fab228d2a56d43b4471be28bdc2
eliad vertman <eliad.vertman@nice.com>
2019-02-14 19:22:38 +0200

    working on reservation

--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {TablesLogicService} from '../tables-logic.service'
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
   styleUrls: ['./side-navbar.component.css']
 })
 export class SideNavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private tablesLogic : TablesLogicService) { }
 
   ngOnInit() {
-    
+  
      
   
   }
+  name : string
+  time : number
+  attendies : number
+
+  onReservation(){
+    console.log(this.tablesLogic.tables)
+
+  }
+  
 }
